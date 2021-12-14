a2407667d4f6edc15ad2495c5b36d49873a95eb1
Ziyuan Zhao <ziyuanzhao@college.harvard.edu>
2021-12-14 14:20:34 -0500

    add fork to the server thread for executing commands

--- client.cc
+++ client.cc
@@ -1,10 +1,11 @@
 #include "helpers.hh"
+#include <cstdio>
 #include <netdb.h>
 
 // usage: -h [host] -p [port]
 int main(int argc, char** argv) {
-    const char* host = "10.250.64.177"; // use the server's ip here.
-    const char* port = "6162";
+    const char* host = "127.0.0.1"; // use the server's ip here.
+    const char* port = "6163";
 
     // parse arguments
     int opt;
@@ -22,9 +23,9 @@ int main(int argc, char** argv) {
     hints.ai_family = AF_UNSPEC;        // use IPv4 or IPv6
     hints.ai_socktype = SOCK_STREAM;    // use TCP
     hints.ai_flags = AI_NUMERICSERV;
-    // if (strcmp(host, "cs61") == 0) {
-    //     host = "cs61.seas.harvard.edu";
-    // }
+    if (strcmp(host, "ngrok") == 0) {   // Not sure how to fix the dynamic address yet
+        host = "2.tcp.ngrok.io";
+    }
     int r = getaddrinfo(host, port, &hints, &ais);
     if (r != 0) {
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(r));
@@ -54,6 +55,11 @@ int main(int argc, char** argv) {
 
     // 
     printf("Connection established at fd %d\n", fd);
-
+    
+    char buf[1];
+    while (true) {
+        int r = read(fd, buf, 1);
+        if(r == 1) printf("%c", *buf);
+    }
     
 }
--- server.cc
+++ server.cc
@@ -1,16 +1,49 @@
 #include "helpers.hh"
+#include <cassert>
+#include <cstdio>
+#include <string>
+#include <sys/wait.h>
+#include <unistd.h>
 
 void handle_connection(int cfd, std::string remote) {
     (void) remote;
+
+    FILE* fin = fdopen(cfd, "r");  
+    FILE* fout = fdopen(cfd, "w");
+    char buf[BUFSIZ], key[BUFSIZ];
+
     printf("Received incoming connection at cfd: %d\n", cfd);
     usleep(1000000);
+
+
+    while (fgets(buf, BUFSIZ, fin)) {
+        if(sscanf(buf, "echo %s ", key) == 1){
+            pid_t p = fork();
+            if(p == 0) {
+                dup2(cfd, 1);
+                const char *args[3];
+                args[0] = "echo";
+                args[1] = key;
+                args[2] = NULL;
+                execvp("echo", (char **)args);
+                perror("Failed to execute the program!");
+            }
+            assert(p > 0);
+            int status;
+            pid_t exitpid = waitpid(p, &status, 0);
+            assert(exitpid == p);
+            assert(WIFEXITED(status));
+        }
+    }
+
+
     printf("Exiting\n");
+    close(cfd);
 }
 
 
 int main(int argc, char** argv) {
-    // Usage: ./weensydb [PORT]
-    int port = 6162;
+    int port = 6163;
     if (argc >= 2) {
         port = strtol(argv[1], nullptr, 0);
         assert(port > 0 && port <= 65535);
